77b029c5da78f1e00f00de13ce6b52b66bee6133
Kevin <dev@kevinlazich.com>
2019-12-19 09:25:51 -0500

    Complete Day3

--- calendar/day3.js
+++ calendar/day3.js
@@ -2,7 +2,7 @@ import input from '../input/day3'
 
 // Day 3: Crossed Wires
 // 1. 225
-// 2.
+// 2. 35194
 
 const [wire1, wire2] = input
 
@@ -28,44 +28,24 @@ const path = function*(instructions) {
   }
 }
 
-const path1 = new Set()
-const path2 = new Set()
+const path1 = new Map()
+const path2 = new Map()
+let cnt = 0
 
-for (let co of path(wire1)) path1.add(co.join(','))
-for (let co of path(wire2)) path2.add(co.join(','))
+for (let co of path(wire1)) {
+  cnt += 1
+  path1.set(co.join(','), cnt)
+}
+cnt = 0
+for (let co of path(wire2)) {
+  cnt += 1
+  path2.set(co.join(','), cnt)
+}
 
-let intersects1 = new Set()
+let intersects = new Set()
 
-path1.forEach(v => {
-  if (path2.has(v)) intersects1.add(v)
+path1.forEach((v, k) => {
+  if (path2.has(k)) intersects.add(path1.get(k) + path2.get(k))
 })
 
-let intersects2 = new Set()
-
-path2.forEach(v => {
-  if (path1.has(v)) intersects2.add(v)
-})
-
-console.log(intersects1)
-console.log(intersects2)
-
-const weighted = [...intersects1].map((s, i) => [
-  s,
-  i + [...intersects2].indexOf(s),
-])
-console.log(weighted)
-
-// 789 286
-
-let result = 0
-
-for (let co of path1) {
-  result += 1
-  if (co === '789,286') break
-}
-for (let co of path2) {
-  result += 1
-  if (co === '789,286') break
-}
-console.log(result)
-
+console.log(Math.min(...intersects))
