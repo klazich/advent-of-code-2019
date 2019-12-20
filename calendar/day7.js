f0dd4fe6023fc03178a155317e3382192735f2b2
Kevin <dev@kevinlazich.com>
2019-12-20 12:16:56 -0500

    Update Day7

--- calendar/day7.js
+++ calendar/day7.js
@@ -6,4 +6,16 @@ import input from '../input/day7'
 
 import buildIntcode from '../intcode'
 
-const intcode = buildIntcode(intcode)
+const computer = buildIntcode(input)
+
+const combos = function*(iterable, c) {
+  if (iterable.length === 0) yield c
+  for (let i = 0; i < iterable.length; i += 1) {
+    yield* combos(
+      iterable.filter((_, j) => j !== i),
+      [...c, iterable[i]]
+    )
+  }
+}
+
+for (let c of combos([0, 1, 2, 3], [])) console.log(c)
