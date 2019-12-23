6fcdabd56aeb229011c838cffba232b7b6bceaf5
Kevin <dev@kevinlazich.com>
2019-12-23 17:09:08 -0500

    Continue work on Day7

--- calendar/day7.js
+++ calendar/day7.js
@@ -1,14 +1,28 @@
+// @ts-check
 import input from '../input/day7'
 
 // Day 7: Amplification Circuit
-// 1.
+// 1. 92663
 // 2.
 
 import buildIntcode from '../intcode'
+import init from '../intcode/gen'
 
 const computer = buildIntcode(input)
 
-const combos = function*(iterable, c) {
+/** @type {(fn : (a: [number, number]) => number) => (init?: number) => (seq: number[]) => number} */
+const chain = fn => (init = 0) => seq => {
+  let next = init
+  for (let x of seq) next = fn([x, next])
+  return next
+}
+
+/**
+ * @param {any[]} iterable
+ * @param {any[]} [c=[]]
+ * @returns {Generator<number[]>}
+ */
+const combos = function*(iterable, c = []) {
   if (iterable.length === 0) yield c
   for (let i = 0; i < iterable.length; i += 1) {
     yield* combos(
@@ -18,4 +32,54 @@ const combos = function*(iterable, c) {
   }
 }
 
-for (let c of combos([0, 1, 2, 3], [])) console.log(c)
+/**
+ * @param {(arr: number[]) => number} fn
+ * @param {number[]} seq
+ */
+const runAmpDiagnostic = (fn, seq) => {
+  const seqFn = combos(seq)
+  let highest = 0
+  for (let arr of seqFn) {
+    const result = fn(arr)
+    if (result > highest) highest = result
+    console.log(result, arr)
+  }
+  return highest
+}
+
+const ampDiagnostic = chain(computer)()
+
+// const result = runAmpDiagnostic(ampDiagnostic, [0, 1, 2, 3, 4])
+// console.log(result)
+
+init([
+  3,
+  26,
+  1001,
+  26,
+  -4,
+  26,
+  3,
+  27,
+  1002,
+  27,
+  2,
+  27,
+  1,
+  27,
+  26,
+  27,
+  4,
+  27,
+  1001,
+  28,
+  -1,
+  28,
+  1005,
+  28,
+  6,
+  99,
+  0,
+  0,
+  5,
+])([9, 8, 7, 6, 5])
