143391f07e723f6d59b23bf5155d8955de5ee321
Kevin <dev@kevinlazich.com>
2020-01-29 17:54:46 -0500

    add BuildASCII to Intcode dir

--- intcode/index.js
+++ intcode/index.js
@@ -4,13 +4,13 @@ import parseIntcode from './parseIntcode'
 
 const unblock = () => new Promise(setImmediate)
 
-const next = async (arr, id) => {
-  let t = 0
+const take = async (arr, id) => {
+  // let t = 0
   while (true) {
-    t += 1
+    // t += 1
     if (arr.length > 0) return arr.shift()
     else await unblock()
-    if (t === 10000) console.log('WAITING', id ?? '')
+    // if (t === 10000) console.log('WAITING', id ?? '')
   }
 }
 
@@ -25,13 +25,12 @@ export const circuit = baseMachine => async seq => {
   return io.flat()
 }
 
-// export default intcode => (id, phase) => async (src, trg) => {
-export default intcode => (id, src, trg) => async () => {
+const BuildIntcode = intcode => (id, src, trg) => async () => {
   const log = logger(id)
 
   let state = {
     prog: parseIntcode(intcode),
-    ip: 0, // ip: 2,
+    ip: 0,
     rb: 0,
     opcode: null,
     modes: null,
@@ -39,14 +38,12 @@ export default intcode => (id, src, trg) => async () => {
     output: null,
   }
 
-  // state.prog[state.prog[1]] = phase
-
   while (state.prog[state.ip] !== 99) {
     state = parseOpcode(state)
 
     if (state.opcode === 3) {
-      const take = await next(src, id)
-      state.input.push(take)
+      const input = await take(src, id)
+      state.input.push(input)
     }
 
     // state = exec(state, log)
@@ -56,8 +53,40 @@ export default intcode => (id, src, trg) => async () => {
       trg.push(state.output)
     }
 
-    // await unblock()
+    await unblock()
   }
 
-  trg.push('END')
+  trg.push(null)
 }
+
+export const BuildASCII = intcode => (id, src, trg) => async () => {
+  const Intcode = BuildIntcode(intcode)
+  const io = { in: [], out: [] }
+
+  const processInput = (src, trg) => async () => {
+    while (true) {
+      const ins = await take(src)
+      const stream = ins.split('').map(s => s.charCodeAt(0))
+      trg.push(...stream, 10)
+    }
+  }
+
+  const processOutput = (src, trg) => async () => {
+    let str = ''
+    while (true) {
+      const code = await take(src)
+      if (code === 10) {
+        trg.push(str)
+        str = ''
+      } else str += String.fromCharCode(code)
+    }
+  }
+
+  const computer = Intcode(id, io.in, io.out)
+  const input = processInput(src, io.in)
+  const output = processOutput(io.out, trg)
+
+  Promise.all([input(), output(), computer()])
+}
+
+export default BuildIntcode
